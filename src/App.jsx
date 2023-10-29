1db89d63d25bc4f16a934e7a0ce5b3a6d540e32e
Jake Zook <jakezook01@gmail.com>
2023-10-28 20:57:10 -0600

    Basic file structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import logo from "./assets/chatgpt.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="App">
+			<div className="sidebar">
+				<div className="upperSide">
+					<div className="upperSideTop">
+						<img src={logo} alt="logo" className="logo" />
+						<span className="brand">ZookGPT</span>
+						<button className="midBtn">
+							<img src="" alt="Add Chat Button" className="addBtn" />
+							New Chat
+						</button>
+						<div className="upperSideBottom">
+							<button className="query">
+								<img src="" alt="" />
+								What is programming?
+							</button>
+							<button className="query">
+								<img src="" alt="" />
+								What is programming?
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className="lowerSide"></div>
+			</div>
+			<div className="main"></div>
+		</div>
+	);
 }
 
-export default App
+export default App;
